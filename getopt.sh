354949045a7c0ed428f3d7d90bc87b447d66aeb7
Ricky Kwan <rickyk.162@gmail.com>
2022-10-21 20:18:10 -0700

    Update getopt.sh

--- getopt.sh
+++ getopt.sh
@@ -15,6 +15,12 @@ function print_usage() {
 export POSIXLY_CORRECT=1  # Stop parsing at first occurrence of non-option. ie. options are BEFORE the args.
 
 OPTIONS=$(getopt -l "help,foo:,bar:,verbose" -o "vf:b:" -- "$@")
+
+if [ $? -ne 0 ]; then
+    print_usage
+    exit 1
+fi
+
 eval set -- "$OPTIONS"
 while [ $# -ge 1 ]; do
     case "$1" in
@@ -42,4 +48,4 @@ while [ $# -ge 1 ]; do
     shift
 done
 
-echo $1
+echo $#
